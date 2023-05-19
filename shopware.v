d817582215263668e2ca36f4260e4f44a436d034
Thomas Peißl <7underlines@gmail.com>
2023-05-19 07:57:24 +0200

    speed dev

--- shopware.v
+++ shopware.v
@@ -246,7 +246,7 @@ pub fn (mut l Login) sync_upsert(entity string, data []string) {
 	chunks := arrays.chunk(data, 250) // split into chunks
 	for i, chunk in chunks {
 		if i > 0 {
-			time.sleep(2 * time.second)
+			time.sleep(1 * time.second)
 		}
 		c := chunk.filter(it != '')
 		sync_data := '{"v-sync-${entity}":{"entity":"${entity}","action":"upsert","payload":[' +

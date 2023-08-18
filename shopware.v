650de7a3b0ec700c390098620c4290ff6d2cc731
Thomas Peißl <7underlines@gmail.com>
2023-08-18 08:58:52 +0200

    retry success message

--- shopware.v
+++ shopware.v
@@ -248,10 +248,10 @@ pub fn (mut l Login) sync(data string) !string {
 
 // sync_upsert is a shorthand function for sync with data chunking for large arrays
 pub fn (mut l Login) sync_upsert(entity string, data []string) {
-	chunks := arrays.chunk(data, 200) // split into chunks
+	chunks := arrays.chunk(data, 190) // split into chunks
 	for i, chunk in chunks {
 		if i > 0 {
-			time.sleep(3 * time.second)
+			time.sleep(4 * time.second)
 		}
 		c := chunk.filter(it != '')
 		sync_data := '{"v-sync-${entity}":{"entity":"${entity}","action":"upsert","payload":[' +
@@ -266,6 +266,7 @@ pub fn (mut l Login) sync_upsert(entity string, data []string) {
 					eprintln('sync upsert also failed on retry - error: ${err} - giving up')
 					return
 				}
+				println('retry successful')
 			}
 		}
 	}

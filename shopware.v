41a16d79d813e0a3f85ea5db4f179cd3c8912c09
7underlines <thomas.peissl@logmedia.at>
2025-01-21 09:28:38 +0100

    fix: update chunk size in sync_delete function from 400 to 100

--- shopware.v
+++ shopware.v
@@ -340,7 +340,7 @@ pub fn (mut l Login) sync_upsert(entity string, data []string, params ChunkParam
 
 // sync_delete is a shorthand function for sync with data chunking for large arrays
 pub fn (mut l Login) sync_delete(entity string, data []string) {
-	chunks := arrays.chunk(data, 400) // split into chunks
+	chunks := arrays.chunk(data, 100) // split into chunks
 	for chunk in chunks {
 		c := chunk.filter(it != '')
 		sync_data := '{"v-sync-${entity}":{"entity":"${entity}","action":"delete","payload":[' +
